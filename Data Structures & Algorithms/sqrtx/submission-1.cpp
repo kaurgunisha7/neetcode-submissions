a7d8951db73cbfbba63b122abf7a430abefff3bb
kaurgunisha7 <151151636+kaurgunisha7@users.noreply.github.com>
2026-07-07 23:43:27 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 0;
+        int high = x;
+        while( low <= high){
+            int mid = (low + high)/2;
+            long long mul = 1LL * mid * mid;
+            if(mul == x) return mid;
+            else if(mul > x) high = mid - 1;
+            else low = mid+1;
+        }
+        return high;
+    }
+};
