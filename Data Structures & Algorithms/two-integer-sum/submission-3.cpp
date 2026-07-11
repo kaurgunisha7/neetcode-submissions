eae15bef983ad80332cfefa98c9f8eabd7485ecc
kaurgunisha7 <151151636+kaurgunisha7@users.noreply.github.com>
2026-07-11 20:52:29 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            int rem = target - nums[i];
+            if (hash.find(rem) != hash.end()) {
+                return {hash[rem], i};
+            }
+
+            hash[nums[i]] = i;
+        }
+        return {};
+    }
+};
